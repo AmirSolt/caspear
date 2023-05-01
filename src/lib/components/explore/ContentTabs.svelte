ed3d59723d319a3ffc866a6f2fcc05a5ff305a51
AmirSolt <soltamir194@gmail.com>
2023-05-01 05:37:03 -0400

    changed contentRadio to contentTabs}

---
+++ src/lib/components/explore/ContentTabs.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+    import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+
+
+    export const contentTabs:string[] = ['Brands', 'Events'];
+    export let chosenContentTab:string = contentTabs[0];
+
+</script>
+
+
+
+
+<TabGroup>
+    {#each contentTabs as tab}
+        <Tab bind:group={chosenContentTab} name={tab} value={tab}>{tab}</Tab>
+    {/each}
+
+    
+
+</TabGroup>
+			
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,37 +4,41 @@
 
 	import BrandCard from '$lib/components/cards/BrandCard.svelte';
 	import EventCard from '$lib/components/cards/EventCard.svelte';
-	import ContentRadio from '$lib/components/explore/ContentRadio.svelte';
 	import CategorySelect from '$lib/components/explore/CategorySelect.svelte';
 	import Sortby from '$lib/components/explore/Sortby.svelte';
+	import ContentTabs from '$lib/components/explore/ContentTabs.svelte';
 
-	let contentRadio: number  = 0;
+    const contentTabs:string[] = ["Brands", "Events"];
+    let chosenContentTab:string = contentTabs[0];
 
 
 
 	import { categories, getBrandsByCategoryId, getEventsByCategoryId } from '$lib/data/marketData';
-
 	let chosenCategoryId: string = categories[0].id;
 	$: brands = getBrandsByCategoryId(chosenCategoryId);
 	$: events = getEventsByCategoryId(chosenCategoryId);
 
+
 </script>
 
-<h1>{["Brands", "Events"][contentRadio]}</h1>
+
+<ContentTabs bind:chosenContentTab  contentTabs={contentTabs}/>
+
+
+<h1>{chosenContentTab}</h1>
 
 <CategorySelect bind:chosenCategoryId categories={categories} />
 
 
-<ContentRadio bind:contentRadio />
 <Sortby />
 
-{#if contentRadio==0}
+{#if chosenContentTab=="Brands"}
 	{#each brands as brand}
 		<BrandCard {brand} />
 	{/each}
 {/if}
 
-{#if contentRadio==1}
+{#if chosenContentTab=="Events"}
 	{#each events as event}
 		<EventCard {event} />
 	{/each}
